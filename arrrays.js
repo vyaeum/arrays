905fc82ca119ff6c8073294a97c0e7e4ba14d39f
vyaeum <kroge63@gmail.com>
2025-10-20 11:49:06 +0200

    elements with even indexes

--- arrrays.js
+++ arrrays.js
@@ -24,3 +24,11 @@ console.log("Размер массива :", arrayWithNumbers.length);
 let copyArrayWithNumbers = arrayWithNumbers.slice();
 console.log("Копия массива:", copyArrayWithNumbers);
 console.log("Оригинальный массив:", copyArrayWithNumbers);
+
+for (let i = 0; i < arrayWithNumbers.length; i++) {
+  if (i % 2 === 0) {
+    console.log(arrayWithNumbers[i]);
+  }
+}
+
+
