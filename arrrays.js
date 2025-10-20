fd3c3981a431c17411494a51fe86c113524ff2a7
vyaeum <kroge63@gmail.com>
2025-10-20 13:26:54 +0200

    average price and amount of phones with diff ram

--- arrrays.js
+++ arrrays.js
@@ -37,3 +37,51 @@ for (let i = 0; i < arrayWithNumbers.length; i++) {
 }
 
 console.log("Добуток:", accumForCount);
+
+function Phone(id, brand, model, color, ram, price) {
+  this.id = id;
+  this.brand = brand;
+  this.model = model;
+  this.color = color;
+  this.ram = ram;
+  this.price = price;
+}
+
+const phones = [];
+
+const PHONES_COUNT = 30;
+
+for (let i = 0; i < PHONES_COUNT; i++) {
+  const phone = new Phone(
+    i,
+
+    Math.random() > 0.5 ? "Samsung" : "Iphone",
+
+    `Series ${Math.trunc(Math.random() * 8)}`,
+    Math.random() > 0.5 ? "white" : "black",
+    1 + Math.trunc(Math.random() * 12),
+    5000 + Math.trunc(Math.random() * 7000)
+  );
+  phones.push(phone);
+}
+
+let ramCount = { 4: 0, 6: 0, 8: 0, 12: 0 };
+
+for (let i = 0; i < phones.length; i++) {
+  let ram = phones[i].ram;
+
+  if (ram === 4 || ram === 6 || ram === 8 || ram === 12) {
+    ramCount[ram]++;
+  }
+}
+console.log(ramCount);
+
+let totalPrice = 0
+
+for (let i = 0; i < phones.length; i++) {
+  totalPrice += phones[i].price;
+  }
+
+  let averagePrice = totalPrice / phones.length;
+
+  console.log("Средняя цена:", averagePrice.toFixed(0), "грн");
