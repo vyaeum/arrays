6fb70e4a341e242f9e5c66600fa15b2066043510
vyaeum <kroge63@gmail.com>
2025-10-20 11:10:37 +0200

    create a new array with random numbers

---
+++ arrrays.js
@@ -0,0 +1,8 @@
+const arrayWithNumbers = [];
+
+for (let i = 0; i < 10; i++) {
+  const randomNumber = Math.floor(Math.random() * 100);
+  arrayWithNumbers.push(randomNumber);
+}
+
+console.log(arrayWithNumbers)
